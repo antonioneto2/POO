994bb859ec564fa7da2772f70c726541b08e1688
Antonio Neto <antonio.nog28@gmail.com>
2025-03-06 11:37:58 -0300

    Resolução questão 9

--- classes/Lista/q09/Complexo.java
+++ classes/Lista/q09/Complexo.java
@@ -1,5 +1,26 @@
 package classes.Lista.q09;
 
 public class Complexo {
+    float real;
+    float imaginario;
+    
+    public Complexo(float real, float imaginario) {
+        this.real = real;
+        this.imaginario = imaginario;
+    }
 
+    public Complexo(float real){
+        this.real = real;
+        this.imaginario = 0.0F;
+    }
+
+    public Complexo(){
+        this.real = 0.0F;
+        this.imaginario= 0.0F;
+    }
+
+    public String toString(){
+        return (real + " + " + imaginario + "i");
+    }
+    
 }
---
+++ classes/Lista/q09/DemoComplexo.java
@@ -0,0 +1,12 @@
+package classes.Lista.q09;
+
+public class DemoComplexo {
+    public static void main(String[] args) {
+        Complexo n1 = new Complexo(3, 7);
+        System.out.println(n1.toString());
+        Complexo n2 = new Complexo(7);
+        System.out.println(n2.toString());
+        Complexo n3 = new Complexo();
+        System.out.println(n3.toString());
+    }
+}
