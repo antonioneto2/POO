767401c2feb812264f13cfe6f680f7abdb3aa88b
Antonio Neto <antonio.nog28@gmail.com>
2025-03-06 16:32:59 -0300

    Outra forma de tipos genericos

--- classes/igualdade/Igual.java
+++ classes/igualdade/Igual.java
@@ -1,17 +1,8 @@
 package classes.igualdade;
 
 public class Igual {
-    public static boolean ehIgual(Double d1, Double d2){
+    public static <T> boolean ehIgual(T d1, T d2){
         return d1.equals(d2);
     }
-
-    public static boolean ehIgual(String s1, String s2){
-        return s1.equals(s2);
-    }
-
-    public static boolean ehIgual(Integer i1, Integer i2){
-        return i1.equals(i2);
-    }
-    
 
 }
