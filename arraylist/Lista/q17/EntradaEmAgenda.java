7765e7a1e77e66e49c349eace6de1a7af40be6c0
Antonio Neto <antonio.nog28@gmail.com>
2025-03-11 17:30:06 -0300

    criando primeira classe questão 17

---
+++ arraylist/Lista/q17/EntradaEmAgenda.java
@@ -0,0 +1,16 @@
+package arraylist.Lista.q17;
+
+public class EntradaEmAgenda {
+    private int dia, mes, ano;
+    private float hora;
+    private String assunto;
+    
+    public EntradaEmAgenda(int dia, int mes, int ano, float hora) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.hora = hora;
+    }
+    
+    
+}
