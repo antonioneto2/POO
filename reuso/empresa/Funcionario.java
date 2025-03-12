64e0cc15cca0a316886d76cd2434441abd29693f
Antonio Neto <antonio.nog28@gmail.com>
2025-03-12 17:06:08 -0300

    Implementando classe Funcionario

---
+++ reuso/empresa/Funcionario.java
@@ -0,0 +1,16 @@
+package reuso.empresa;
+
+public class Funcionario extends Pessoa {
+    private float salario;
+
+    public Funcionario(String nome, String identidade, float salario) {
+        super(nome, identidade);
+        this.salario = salario;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    
+}
