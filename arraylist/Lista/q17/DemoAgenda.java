5f9b5696458c3712db21dafa530ceb5290644a66
Antonio Neto <antonio.nog28@gmail.com>
2025-03-12 14:47:28 -0300

    Questão 17 resolvida

--- arraylist/Lista/q17/DemoAgenda.java
+++ arraylist/Lista/q17/DemoAgenda.java
@@ -2,6 +2,12 @@ package arraylist.Lista.q17;
 
 public class DemoAgenda {
     public static void main(String[] args) {
-        
+        Agenda agenda = new Agenda();
+
+        agenda.adicionarCompromisso(new EntradaEmAgenda(28 , 2, 2025, 19, "Aniversário de 20 anos!"));
+        agenda.adicionarCompromisso(new EntradaEmAgenda(28 , 2, 2025, 10, "Consulta medica"));
+        agenda.adicionarCompromisso(new EntradaEmAgenda(10 , 1, 2025, 0, "Aniversário de 1 ano de namoro!"));
+
+        agenda.listaDia(28, 2, 2025);
     }
 }
