0613e029490fe2b01db39f53a6711a3a67726211
Antonio Neto <antonio.nog28@gmail.com>
2025-03-12 16:56:44 -0300

    Pratica empresa, add classe pessoa

---
+++ reuso/empresa/Pessoa.java
@@ -0,0 +1,27 @@
+package reuso.empresa;
+
+public class Pessoa {
+    private String nome;
+    private String identidade;
+    
+    public Pessoa(String nome, String identidade) {
+        this.nome = nome;
+        this.identidade = identidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdentidade() {
+        return identidade;
+    }
+
+    @Override
+    public String toString() {
+        return "nome=" + nome + ", identidade=" + identidade;
+    }
+
+    
+    
+}
