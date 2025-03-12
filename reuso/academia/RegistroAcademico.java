302e8733d752059033acf7438a088cb24e015f4d
Antonio Neto <antonio.nog28@gmail.com>
2025-03-12 16:24:57 -0300

    Criando classes para a pratica Academia

---
+++ reuso/academia/RegistroAcademico.java
@@ -0,0 +1,20 @@
+package reuso.academia;
+
+public class RegistroAcademico {
+    private String nomeDoAulno;
+    private String matricula;
+    private String curso;
+    
+    public RegistroAcademico(String nomeDoAulno, String matricula, String curso) {
+        this.nomeDoAulno = nomeDoAulno;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String toString() {
+        return "Nome: " + nomeDoAulno + "; "+ "Matricula: " + matricula + "; "+ "Curso: " + curso;
+    }
+    
+
+
+}
---
+++ reuso/academia/RegistroAcademicoPosGraduacao.java
@@ -0,0 +1,5 @@
+package reuso.academia;
+
+public class Registro {
+
+}
